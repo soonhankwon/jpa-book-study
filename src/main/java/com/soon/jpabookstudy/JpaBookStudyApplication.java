24bbec6e6656c82bbf553261d2bd96b979066cfb
soonable@naver.com <soonable@naver.com>
2023-05-22 11:18:05 +0900

    init

---
+++ src/main/java/com/soon/jpabookstudy/JpaBookStudyApplication.java
@@ -0,0 +1,13 @@
+package com.soon.jpabookstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBookStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBookStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soon/jpabookstudy/JpaBookStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soon.jpabookstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBookStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
